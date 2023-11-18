2e1d2705b284ac911a55346cf029028eb8744055
INTERNAL\ionut.galan <ionut.galan@visma.com>
2023-11-18 16:13:13 +0200

    Define the seed of the project usign spring boot

---
+++ FibonnaciAPI/src/main/java/com/smartbill/FibonnaciAPI/FibonnaciApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartbill.FibonnaciAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonnaciApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonnaciApiApplication.class, args);
+	}
+
+}
---
+++ FibonnaciAPI/src/test/java/com/smartbill/FibonnaciAPI/FibonnaciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbill.FibonnaciAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibonnaciApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
